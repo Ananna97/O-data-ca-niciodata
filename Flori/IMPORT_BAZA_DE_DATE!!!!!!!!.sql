7bfd4a7232e87c3f4ad2d913a8f23d9849f4db62
Ananna97 <38731739+Ananna97@users.noreply.github.com>
2019-05-10 00:07:47 +0300

    Update IMPORT_BAZA_DE_DATE!!!!!!!!.sql

--- Flori/IMPORT_BAZA_DE_DATE!!!!!!!!.sql
+++ Flori/IMPORT_BAZA_DE_DATE!!!!!!!!.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2019 at 11:32 PM
+-- Generation Time: May 09, 2019 at 11:05 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -51,7 +51,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `order_products` (
   `order_id` int(10) UNSIGNED NOT NULL,
-  `CNP` bigint(20) UNSIGNED DEFAULT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL,
   `id_product` bigint(20) UNSIGNED DEFAULT NULL,
   `quantity` int(10) UNSIGNED NOT NULL,
   `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -66,14 +66,14 @@ CREATE TABLE `order_products` (
 -- Dumping data for table `order_products`
 --
 
-INSERT INTO `order_products` (`order_id`, `CNP`, `id_product`, `quantity`, `address`, `city`, `county`, `postal_code`, `created_at`, `updated_at`) VALUES
-(1, 2940521430997, 10001, 3, 'Romanitei, nr.47', 'Prahova', 'Campina', 105600.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
-(2, 2960227357879, 40005, 1, 'Aleea Migdalelor, nr.5', 'Bucuresti', 'Bucuresti', 222444.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
-(3, 1951214519263, 60006, 2, 'Baiului, nr.7', 'Alba-Iulia', 'Alba', 234345.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
-(4, 1951214519263, 60005, 3, 'Baiului, nr.7', 'Alba-Iulia', 'Alba', 234345.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
-(5, 1910527384590, 10001, 1, 'Castelului, nr.14', 'Bucuresti', 'Bucuresti', 189101.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
-(6, 2960227357879, 60002, 7, 'Aleea Migdalelor, nr.5', 'Bucuresti', 'Bucuresti', 222444.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
-(7, 2940521430997, 50003, 2, 'Romanitei, nr.47', 'Prahova', 'Campina', 105600.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16');
+INSERT INTO `order_products` (`order_id`, `user_id`, `id_product`, `quantity`, `address`, `city`, `county`, `postal_code`, `created_at`, `updated_at`) VALUES
+(1, 3, 10001, 3, 'Romanitei, nr.47', 'Prahova', 'Campina', 105600.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
+(2, 4, 40005, 1, 'Aleea Migdalelor, nr.5', 'Bucuresti', 'Bucuresti', 222444.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
+(3, 2, 60006, 2, 'Baiului, nr.7', 'Alba-Iulia', 'Alba', 234345.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
+(4, 2, 60005, 3, 'Baiului, nr.7', 'Alba-Iulia', 'Alba', 234345.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
+(5, 1, 10001, 1, 'Castelului, nr.14', 'Bucuresti', 'Bucuresti', 189101.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
+(6, 4, 60002, 7, 'Aleea Migdalelor, nr.5', 'Bucuresti', 'Bucuresti', 222444.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16'),
+(7, 3, 50003, 2, 'Romanitei, nr.47', 'Prahova', 'Campina', 105600.00, '2019-04-17 22:15:16', '2019-04-17 22:15:16');
 
 -- --------------------------------------------------------
 
@@ -143,7 +143,7 @@ INSERT INTO `products` (`id_product`, `name_product`, `slug`, `description`, `pr
 --
 
 CREATE TABLE `users` (
-  `CNP` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
   `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last-name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -159,11 +159,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`CNP`, `first_name`, `last-name`, `username`, `email`, `password`, `telephone`, `submit_check`, `created_at`, `updated_at`) VALUES
-(1910527384590, 'Alexandru', 'Crisan', 'crtyalex12', 'uiiuiuhuhui@yahoo.com', 'crisal123', '0755544312', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29'),
-(1951214519263, 'Ion', 'Petrescu', 'ion123', 'Ion.Petrescu@yahoo.com', 'ion123', '0711223344', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29'),
-(2940521430997, 'Clara', 'Caramitru', 'clara12', 'Caramitru.Clara@yahoo.com', 'claral123', '0733367589', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29'),
-(2960227357879, 'Cristina', 'Stanescu', 'crtinas123', 'Stanescu.Cristina@yahoo.com', 'cristinal123', '0787654321', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29');
+INSERT INTO `users` (`user_id`, `first_name`, `last-name`, `username`, `email`, `password`, `telephone`, `submit_check`, `created_at`, `updated_at`) VALUES
+(1, 'Alexandru', 'Crisan', 'crtyalex12', 'uiiuiuhuhui@yahoo.com', 'crisal123', '0755544312', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29'),
+(2, 'Ion', 'Petrescu', 'ion123', 'Ion.Petrescu@yahoo.com', 'ion123', '0711223344', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29'),
+(3, 'Clara', 'Caramitru', 'clara12', 'Caramitru.Clara@yahoo.com', 'claral123', '0733367589', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29'),
+(4, 'Cristina', 'Stanescu', 'crtinas123', 'Stanescu.Cristina@yahoo.com', 'cristinal123', '0787654321', '1', '2019-04-17 21:33:29', '2019-04-17 21:33:29');
 
 --
 -- Indexes for dumped tables
@@ -181,8 +181,8 @@ ALTER TABLE `migrations`
 ALTER TABLE `order_products`
   ADD PRIMARY KEY (`order_id`),
   ADD UNIQUE KEY `order_products_order_id_unique` (`order_id`),
-  ADD KEY `order_products_cnp_foreign` (`CNP`),
-  ADD KEY `order_products_id_product_foreign` (`id_product`);
+  ADD KEY `order_products_id_product_foreign` (`id_product`),
+  ADD KEY `order_products_user_id_foreign` (`user_id`) USING BTREE;
 
 --
 -- Indexes for table `products`
@@ -196,8 +196,8 @@ ALTER TABLE `products`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`CNP`),
-  ADD UNIQUE KEY `users_cnp_unique` (`CNP`),
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `users_cnp_unique` (`user_id`),
   ADD UNIQUE KEY `users_username_unique` (`username`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
@@ -227,7 +227,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `CNP` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483647;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -237,8 +237,13 @@ ALTER TABLE `users`
 -- Constraints for table `order_products`
 --
 ALTER TABLE `order_products`
-  ADD CONSTRAINT `order_products_cnp_foreign` FOREIGN KEY (`CNP`) REFERENCES `users` (`CNP`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `order_products_id_product_foreign` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `order_products` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
