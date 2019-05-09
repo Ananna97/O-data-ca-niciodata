f5e51f7bf9fb6c8cdd2f099bcdb70b9cbb89334e
Ananna97 <38731739+Ananna97@users.noreply.github.com>
2019-05-10 00:45:02 +0300

    d

--- Flori/database/seeds/UsersTableSeeder.php
+++ Flori/database/seeds/UsersTableSeeder.php
@@ -19,7 +19,7 @@ class UsersTableSeeder extends Seeder
             'last-name'=>'Crisan', 
             'username'=>'crtyalex12',
             'email'=>'uiiuiuhuhui@yahoo.com', 
-            'password'=>'crisal123',
+            'password'=>'$2y$10$WQKCk/.0OjHqAOnXWAtu/eLfqsB1ZNGdTiCkqOluPLqI58PkhpddS',
             'telephone'=> '0755544312',
             'submit_check'=>'1'
         ]);
@@ -30,7 +30,7 @@ class UsersTableSeeder extends Seeder
             'last-name'=>'Petrescu', 
             'username'=>'ion123',
             'email'=>'Ion.Petrescu@yahoo.com', 
-            'password'=>'ion123',
+            'password'=>'$2y$10$lUOrtM4GSOP.fwsTDxF20ONasxFYvCQCyTdHi940pRkWO5wSFKlI2',
             'telephone'=>'0711223344',
             'submit_check'=>'1'
         ]);
@@ -41,7 +41,7 @@ class UsersTableSeeder extends Seeder
             'last-name'=>'Caramitru', 
             'username'=>'clara12',
             'email'=>'Caramitru.Clara@yahoo.com', 
-            'password'=>'claral123',
+            'password'=>'$2y$10$XYCq9sHsPmibEyU4wMc5uOGn6q9/EykvqJGjJo4XRfJMcd4W1gpj.',
             'telephone'=>'0733367589',
             'submit_check'=>'1'
         ]);
@@ -52,7 +52,7 @@ class UsersTableSeeder extends Seeder
             'last-name'=>'Stanescu', 
             'username'=>'crtinas123',
             'email'=>'Stanescu.Cristina@yahoo.com', 
-            'password'=>'cristinal123',
+            'password'=>'$2y$10$cSR6mWJa1R9vvHIcwDVzFu0GQHtBZKjh07nhZQctTNaAqNozaT/Ya',
             'telephone'=>'0787654321',
             'submit_check'=>'1'
         ]);
